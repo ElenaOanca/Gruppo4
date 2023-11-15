a70ff4df09daf3375eb1399a7f7b71e4bb1fc031
samuelet23 <samueletoscano@hotmail.com>
2023-11-15 09:27:41 +0100

    aggiunt afunzione al click della search page

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
 const QUERY_URL = "https://api.spotify.com/v1/search?q=";
-const token = "Bearer BQChn4wkik5G4NmflPRJhVbBJDZsBWfvHwYBtQ4qMNmmlN9bVgAM374jvTubU0ZUIU57bI2KBaX2t4S8wtZnhx7mNUN4BoPNpCeq_XZQ6ADus5L-u1I"
+const token = "Bearer BQCssO8UTDbIV-9A75hRAMwrlXj252jt_BzrrXpQXlKJWzAsqNPpGT8p2Z9-RoGQvzgAAOzms4Sbq8lqjU_IwV8GkTqk_wuGxQYuZu9gzUmLaMR0cl0"
 
 const searchBox = document.querySelector('.searchForm input');
 
@@ -21,18 +21,21 @@ searchBox.addEventListener('keydown', async (e) => {
         console.log(results);
         let target = document.querySelector('.targetSearch')
         let clone = cloneSearchResults()
-        let container = clone.querySelector('.container-risultati')
         let img = clone.querySelector('.img-risultati')
         let artistName = clone.querySelector('.nomi-risultati')
         let btn = clone.querySelector('.btn-risultati')
     
         for (let i = 0 ; i <6 ; i++) {
-            //alcune non hanno immagini, risolvere questo problema
+           if (img.src = results.artists.items[i].images[0].url) {
             img.src = results.artists.items[i].images[0].url
+           } else{
+            img.src = 'https://play-lh.googleusercontent.com/eN0IexSzxpUDMfFtm-OyM-nNs44Y74Q3k51bxAMhTvrTnuA4OGnTi_fodN4cl-XxDQc'
+           }
             artistName.innerText = `Ascolta ${results.artists.items[i].name}`
             btn.href = results.artists.items[i].uri
             btn.innerText = 'Ascolta ora'
             target.append(clone)
+            searchBox.value =''
         }
 
     } else {
