20e4331967e53d9379caf47be4378ff197d22d6e
samuelet23 <samueletoscano@hotmail.com>
2023-11-16 09:28:20 +0100

    search page completa

--- js/search.js
+++ js/search.js
@@ -1,9 +1,10 @@
 const url = "https://api.spotify.com/v1/search?q=";
 const token = "Bearer BQCJU3D_eApnQ10XkmL6KrQLv6s91RGO3h-g_ldtPSBM60FSXDnSlFZ2yMHhlZpxSrs26JSO9d7jYr3YWywj19kUQrOocHOlU1pp_yAJvXbZASvIA4M"
+const newAlbumUrl = "https://api.spotify.com/v1/browse/new-releases"
 
 const searchBox = document.querySelector('.searchForm input');
 
-
+//query aritsta
 async function searchByQueryArtist (query) {
     return await fetch (`${url}${query}&type=artist` , {
         headers : {
@@ -13,29 +14,7 @@ async function searchByQueryArtist (query) {
         })
     .then(res => res.json())
 }
-
-async function searchByQuerySong(query) {
-    return await fetch (`${url}${query}&type=track` , {
-        headers : {
-            "Content-Type": "application/json",
-            Authorization : token
-        }
-        })
-    .then(res => res.json())
-}
-
-async function searchByQueryAlbum(query) {
-    return await fetch (`${url}${query}&type=album` , {
-        headers : {
-            "Content-Type": "application/json",
-            Authorization : token
-        }
-        })
-    .then(res => res.json())
-}
-
-
-
+//ricerca aritsta
 async function renderArtistsSearch() {
     searchBox.addEventListener('keydown', async (e) => {
         if (e.keyCode === 13) {
@@ -54,7 +33,8 @@ async function renderArtistsSearch() {
                         let img = clone.querySelector('.img-artist');
                         let btn = clone.querySelector('.btn-artist');
                         let name = clone.querySelector('.nome-artist');
-                
+                        let container = clone.querySelectorAll('.single-card');
+                        container.forEach(card => card.style.backgroundColor = getRandomColor());
                         img.src = item.images[0].url;
                         btn.href = "artista.html?id=" + item.id;
                         btn.innerText = 'Ascolta ora';
@@ -69,6 +49,20 @@ async function renderArtistsSearch() {
 }
 
 renderArtistsSearch()
+
+//query song
+
+async function searchByQuerySong(query) {
+    return await fetch (`${url}${query}&type=track` , {
+        headers : {
+            "Content-Type": "application/json",
+            Authorization : token
+        }
+        })
+    .then(res => res.json())
+}
+
+//ricerca canzone
 
 async function renderSongsSearch() {
     searchBox.addEventListener('keydown', async (e) => {
@@ -89,7 +83,8 @@ async function renderSongsSearch() {
                     let img = clone.querySelector('.img-song');
                     let btn = clone.querySelector('.btn-song');
                     let name = clone.querySelector('.nomi-song');
-                    
+                    let container = clone.querySelectorAll('.container-song');
+                    container.forEach(card => card.style.backgroundColor = getRandomColor());
             
                     img.src = item.album.images[0].url;
                     btn.href = ''; // Aggiungi l'URL desiderato
@@ -106,6 +101,19 @@ async function renderSongsSearch() {
 renderSongsSearch()
 
 
+//query album
+
+async function searchByQueryAlbum(query) {
+    return await fetch (`${url}${query}&type=album` , {
+        headers : {
+            "Content-Type": "application/json",
+            Authorization : token
+        }
+        })
+    .then(res => res.json())
+}
+//ricerca album
+
 async function renderAlbumSearch() {
     let target = document.querySelector('.targetSearchAlbum');
    
@@ -120,8 +128,6 @@ async function renderAlbumSearch() {
             let items = album.albums.items;
             console.log(album);
             
-           
-
             for (let index = 0; index < 4; index++) {
                 // Clona il contenuto del template con ID 'risultatoSearchAlbum'
                 let temp = document.querySelector('#risultatoSearchAlbum');
@@ -132,6 +138,8 @@ async function renderAlbumSearch() {
                 let btn = clone.querySelector('.btn-album');
                 let name = clone.querySelector('.nome-album');
                 let releaseDate = clone.querySelector('.releaseDate-album');
+                
+               
                 
                 
                 
@@ -155,43 +163,61 @@ renderAlbumSearch()
 
 
 
+//query newReleases
 
 async function getNewReleases () {
     return await fetch (newAlbumUrl ,
         {
             headers : {
                 "Content-Type": "application/json",
-                Authorization : `Bearer ${token}`   
+                Authorization : token   
             }
         })
     .then(res => res.json())
 }
 
-async function renderNewReleases () {
-let target = document.querySelector('#targetTrendingAlbum')
-let albums = await getNewReleases()
-console.log(albums);
-albums.albums.items.forEach((album, i) => {
-    if (album.album_type == "album"){
-        let clone = cloneTemplate("#areaTrendingALbum")
-        let img1 = clone.querySelector('.image1');
-        let img2 = clone.querySelector('.image2');
-        let title = clone.querySelector('.album-title');
-        let artist = clone.querySelector('.album-artist');
-        // let tracks = clone.querySelector('.tracks');
+async function renderNewReleases() {
+    let albums = await getNewReleases();
+    console.log(albums);
 
-        img1.src = album.images[1].url
-        img2.src = album.images[0].url
-        title.innerText = album.name
-        artist.innerText = album.artists.map(artist => artist.name).join(', ');
-        // tracks.innerText = album.total_tracks
-        target.append(clone)
+    let target = document.querySelector('#targetTrendingAlbum');
+    let temp = document.querySelector('#areaTrendingALbum');
+
+    for (let i = 0; i < albums.albums.items.length; i++) {
+        let album = albums.albums.items[i];
+
+        if (album.album_type === "album") {
+            let clone = temp.content.cloneNode(true);
+            let img1 = clone.querySelector('#image1');
+            let img2 = clone.querySelector('#image2');
+            let title = clone.querySelector('.album-title');
+            let artist = clone.querySelector('.album-artist');
+            let container = clone.querySelectorAll('.single-card');
+            // let tracks = clone.querySelector('.tracks');
+
+            img1.src = album.images[1].url;
+            img2.src = album.images[0].url;
+            title.innerText = album.name;
+            artist.innerText = album.artists.map(artist => artist.name).join(', ');
+            // tracks.innerText = album.total_tracks;
+           container.forEach(card => card.style.backgroundColor = getRandomColor());
+            
+            target.append(clone);
+        }
     }
-})
-
-
 }
 
 renderNewReleases();
 
-  
+function getRandomColor() {
+    // Genera valori RGB casuali
+    let col1 = Math.floor(Math.random() * 256);
+    let col2 = Math.floor(Math.random() * 100);
+    let col3 = Math.floor(Math.random() * 156);
+
+    
+    return `rgb(${col1}, ${col2}, ${col3})`;
+}
+
+
+getRandomColor();
