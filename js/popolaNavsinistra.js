b644cf1dd47c2df201487ce10e768704fc4bd536
Maudit00 <davide9311@gmail.com>
2023-11-17 09:27:46 +0100

    commit fixando token

--- js/popolaNavsinistra.js
+++ js/popolaNavsinistra.js
@@ -1,15 +1,12 @@
 const endpointPlaylist =  'https://api.spotify.com/v1/me/playlists';
 
 
-
-
 async function getAmici() {
     return await fetch(endpointPlaylist,
         {
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer BQAEy0CZo4gf7iBBGWey2FXaGwoYMyraBbchhi8dNZxk6bw4fKbh79cvdS5qOiOUw0D5TL1aQMuuLfRiSYATYMv8SCKwKiEBjRciG0LMV0HKbmPdt3j3PdWQeP9Gpd1RWVyPQW94gWCulhTc-Qsaela7twfnF1axD4PvcQVrU254ZH5U8OQ-cEGwekkvv-Xm1yP_tMM5U6vqGg
-                `
+                Authorization: `Bearer ${token}`
             }
         })
         .then(res => res.json())
